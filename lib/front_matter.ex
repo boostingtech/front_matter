7cf1024d623b3b882b67f1b1dd28bcc7e16b5d1c
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-01-18 22:19:24 -0300

    basic module doc

--- lib/front_matter.ex
+++ lib/front_matter.ex
@@ -1,5 +1,23 @@
 defmodule FrontMatter do
   @moduledoc """
-  Documentation for `FrontMatter`.
+  Parse a file or string containing front matter and a document body.
+  Front matter is a block of yaml wrapped between two lines containing `---`.
+
+  In this example, the front matter contains `title: Hello` and `tags: x, y, z`, and the body is
+  `Hello, world`:
+  ```md
+  ---
+  title: Hello
+  tags: x, y, z
+  ---
+  Hello, world
+  ```
+
+  After parsing the document, front matter is returned as a map, and the body as
+  a string.
+  ```elixir
+  FrontMatter.parse_file "example.md"
+  {:ok, %{"title" => "Hello", "tags" => ["x", "y", "z"]}, "Hello, world"}
+  ```
   """
 end
